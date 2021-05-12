c2332ca5f7b0b28bf27f5a9f4a22048de427b42d
Lipin Nambiar <lipinnambiar@gmail.com>
2021-05-12 14:31:58 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,57 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+  password = '';
+  includLetters = false;
+  inlcudeNumbers = false;
+  includeSymbols = false;
+  passwordLength: number = 0;
+
+  onSelectLetters() {
+    this.includLetters = !this.includLetters;
+  }
+
+  onSelectNumbers() {
+    this.inlcudeNumbers = !this.inlcudeNumbers;
+  }
+
+  onSelectSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onSelectPasswordLength(value: string) {
+    this.passwordLength = parseInt(value);
+  }
+
+  onButtonClick() {
+    let generatedPassword = '';
+
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()_/';
+
+    let validChars = '';
+
+    if (this.includLetters) {
+      validChars += letters;
+    }
+
+    if (this.inlcudeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    for (let index = 0; index <= this.passwordLength; index++) {
+      const indx = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[indx];
+    }
+
+    this.password = generatedPassword;
+  }
 }
